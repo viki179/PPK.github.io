b277eab6276846892caaedb6d29f8f9f3d619d25
viki179 <71555821+viki179@users.noreply.github.com>
2020-11-11 18:06:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Membuat Form Sederhana</title>
-</head>
-    
+<html lang="en">    
 <body>
     <form action="post.php" method="post">
-            <fieldset>
                 <legend>BIODATA: </legend>
                 <label for="">Nama Panjang: </label><br>
                 <input type="text" name="fname"><br><br>
                 <label for="">Email: </label><br>
                 <input type="text" name="lname"><br><br>
                 <button style='font-size:24px'>Gabung <i class='fab fa-github'></i></button>
-            </fieldset>
         </form>
 </body>
 </html>
