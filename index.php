80380f7de794ad481a15c790b4c4d950605bfde1
viki179 <71555821+viki179@users.noreply.github.com>
2020-11-11 17:58:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Sederhana</title>
+</head>
+<body>
+    <form action="post.php" method="post">
+            <fieldset>
+                <legend>BIODATA: </legend>
+                <label for="">Nama Panjang: </label><br>
+                <input type="text" name="fname"><br><br>
+                <label for="">Email: </label><br>
+                <input type="text" name="lname"><br><br>
+                <button style='font-size:24px'>Gabung <i class='fab fa-github'></i></button>
+            </fieldset>
+        </form>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,4 @@
+<?php
+echo 'name  : ' . $_GET['fname'] . '<br/>';
+echo 'email : ' . $_GET['email']
+?>   
